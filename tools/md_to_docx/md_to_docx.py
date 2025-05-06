a4fd83e6ffdd74b34ffe3aa87b397ea860e709a1
fengxiangyuan <fengxiangyuan@wb.com>
2025-05-06 17:03:55 +0800

    feat:first commit

--- tools/md_to_docx/md_to_docx.py
+++ tools/md_to_docx/md_to_docx.py
@@ -10,9 +10,11 @@ from docx import Document
 from docx.oxml.ns import qn
 from docx.shared import RGBColor
 from htmldocx import HtmlToDocx
+from xhtml2pdf import pisa
 
+from tools.utils.md_utils import MarkdownUtils
 from tools.utils.mimetype_utils import MimeType
-from tools.utils.param_utils import get_md_string
+from tools.utils.param_utils import get_md_string, get_meta_data
 
 
 def is_contains_chinese_chars(text: str) -> bool:
@@ -35,30 +37,88 @@ def set_font_color_to_black(doc: Document):
 
     return doc
 
+
+def to_word(answer):
+    html = markdown.markdown(text=answer, extensions=["extra", "toc"])
+    new_parser = HtmlToDocx()
+    doc: Document = new_parser.parse_html_string(html)
+    doc = set_font_color_to_black(doc)
+    if is_contains_chinese_chars(html):
+        set_chinese_fonts(doc)
+    result_bytes_io = io.BytesIO()
+    doc.save(result_bytes_io)
+    result_file_bytes = result_bytes_io.getvalue()
+    return result_file_bytes
+
+
+def to_pdf(answer):
+    html_str = _convert_to_html(answer)
+    result_file_bytes = pisa.CreatePDF(
+        src=html_str.encode("utf-8"),
+        dest_bytes=True,
+        encoding="utf-8",
+    )
+    return result_file_bytes
+
+
 class MarkdownToDocxTool(Tool):
     def _invoke(self, tool_parameters: dict) -> Generator[ToolInvokeMessage, None, None]:
 
         answer = get_md_string(tool_parameters, is_strip_wrapper=True)
-        print(answer)
+        type_content=tool_parameters.get("type")
+        print(type_content)
         try:
+            match type_content:
+                case "pdf":
+                    result_file_bytes = to_pdf(answer)
+                    yield self.create_blob_message(
+                        blob=result_file_bytes,
+                        meta=get_meta_data(
+                            mime_type=MimeType.PDF,
+                            output_filename=tool_parameters.get("测试文档"),
+                        ),
+                    )
+                case "word":
+                    result_file_bytes = to_word(answer)
+                    yield self.create_blob_message(
+                        blob=result_file_bytes,
+                        meta=get_meta_data(
+                            mime_type=MimeType.DOCX,
+                            output_filename=tool_parameters.get("测试文档"),
+                        ),
 
-            html = markdown.markdown(text=answer, extensions=["extra", "toc"])
-
-            new_parser = HtmlToDocx()
-            doc: Document = new_parser.parse_html_string(html)
-            doc = set_font_color_to_black(doc)
-            if is_contains_chinese_chars(html):
-                set_chinese_fonts(doc)
-
-            result_bytes_io = io.BytesIO()
-            doc.save(result_bytes_io)
-            result_file_bytes = result_bytes_io.getvalue()
+                    )
+                case _:
+                    yield self.create_text_message("Failed to convert markdown text to  file ,the type is null ")
         except Exception as e:
             logging.exception("Failed to convert file")
             yield self.create_text_message(f"Failed to convert markdown text to DOCX file, error: {str(e)}")
             return
 
-        yield self.create_blob_message(
-            blob=result_file_bytes,
-            meta={"mime_type": MimeType.DOCX},
-        )
+
+def _convert_to_html(answer: str) -> str:
+    html_str = MarkdownUtils.convert_markdown_to_html(answer)
+        # 字体配置
+    font_families = ",".join(
+        [
+            "Sans-serif",  # for English
+            "STSong-Light",  # for Simplified Chinese
+            "MSung-Light",  # for Traditional Chinese
+            "HeiseiMin-W3",  # for Japanese
+        ]
+    )
+    css_style = f"""
+    <style>
+        html {{
+            -pdf-word-wrap: CJK;
+            font-family:  "{font_families}"; 
+            font-size: 12pt;
+        }}
+    </style>
+    """
+
+    result = f"""
+    {css_style}
+    {html_str}
+    """
+    return result
--- tools/utils/param_utils.py
+++ tools/utils/param_utils.py
@@ -1,6 +1,7 @@
 from typing import Any
-
+from typing import Optional
 from tools.utils.md_utils import MarkdownUtils
+from tools.utils.mimetype_utils import MimeType
 
 
 def get_md_string(tool_parameters: dict[str, Any], is_strip_wrapper: bool = False) -> str:
@@ -12,3 +13,22 @@ def get_md_string(tool_parameters: dict[str, Any], is_strip_wrapper: bool = Fals
         answer = MarkdownUtils.strip_markdown_wrapper(answer)
 
     return answer
+
+def get_meta_data(mime_type: MimeType, output_filename: Optional[str]) -> dict[str, str]:
+    if not MimeType:
+        raise ValueError("Failed to generate meta data, mime_type is not defined")
+
+    # normalize the filename
+    result_filename: Optional[str] = None
+    temp_filename = output_filename.strip() if output_filename else None
+    if temp_filename:
+        # ensure extension name
+        extension = MimeType.get_extension(mime_type)
+        if not temp_filename.lower().endswith(extension):
+            temp_filename = f"{temp_filename}{extension}"
+        result_filename = temp_filename
+
+    return {
+        "mime_type": mime_type,
+        "url": result_filename,
+    }
